5ebf334f6d2e52f053c1afce81aae6cf0cb54254
Dao Lam <daolavi@gmail.com>
2018-08-25 16:50:10 +0700

    Unit test for GetCardType

--- SATA.Tests/CardServiceTest.cs
+++ SATA.Tests/CardServiceTest.cs
@@ -12,8 +12,77 @@ namespace SATA.Tests
 {
     public class CardServiceTest
     {
+        #region GetCardType
         [Fact]
         public void GetCardType_VisaCard()
+        {
+            var options = new DbContextOptionsBuilder<SATADbContext>().UseInMemoryDatabase(databaseName: "TestDb").Options;
+            var dbContext = new SATADbContext(options);
+            var expiryService = A.Fake<IExpiryService>();
+
+            var cardService = new CardService(dbContext, expiryService);
+            var card = new Card
+            {
+                CardNumber = "4123412341234123",
+                ExpiryDate = "102020"
+            };
+            var cardType = cardService.GetCardType(card);
+            Assert.Equal(CardType.Visa, cardType);
+        }
+
+        [Fact]
+        public void GetCardType_MasterCard()
+        {
+            var options = new DbContextOptionsBuilder<SATADbContext>().UseInMemoryDatabase(databaseName: "TestDb").Options;
+            var dbContext = new SATADbContext(options);
+            var expiryService = A.Fake<IExpiryService>();
+
+            var cardService = new CardService(dbContext, expiryService);
+            var card = new Card
+            {
+                CardNumber = "5123412341234123",
+                ExpiryDate = "102020"
+            };
+            var cardType = cardService.GetCardType(card);
+            Assert.Equal(CardType.Master, cardType);
+        }
+
+        [Fact]
+        public void GetCardType_AmexCard()
+        {
+            var options = new DbContextOptionsBuilder<SATADbContext>().UseInMemoryDatabase(databaseName: "TestDb").Options;
+            var dbContext = new SATADbContext(options);
+            var expiryService = A.Fake<IExpiryService>();
+
+            var cardService = new CardService(dbContext, expiryService);
+            var card = new Card
+            {
+                CardNumber = "312341234123412",
+                ExpiryDate = "102020"
+            };
+            var cardType = cardService.GetCardType(card);
+            Assert.Equal(CardType.Amex, cardType);
+        }
+
+        [Fact]
+        public void GetCardType_JCB()
+        {
+            var options = new DbContextOptionsBuilder<SATADbContext>().UseInMemoryDatabase(databaseName: "TestDb").Options;
+            var dbContext = new SATADbContext(options);
+            var expiryService = A.Fake<IExpiryService>();
+
+            var cardService = new CardService(dbContext, expiryService);
+            var card = new Card
+            {
+                CardNumber = "3123412341234123",
+                ExpiryDate = "102020"
+            };
+            var cardType = cardService.GetCardType(card);
+            Assert.Equal(CardType.JCB, cardType);
+        }
+
+        [Fact]
+        public void GetCardType_WrongFormatCard()
         {
             var options = new DbContextOptionsBuilder<SATADbContext>().UseInMemoryDatabase(databaseName: "TestDb").Options;
             var dbContext = new SATADbContext(options);
@@ -28,5 +97,6 @@ namespace SATA.Tests
             var cardType = cardService.GetCardType(card);
             Assert.Equal(CardType.WrongFormatCard,cardType);
         }
+        #endregion
     }
 }
